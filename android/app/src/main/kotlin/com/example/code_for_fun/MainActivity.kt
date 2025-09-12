394e665d525ccfe30729ca74dd6ebbe7c82cc785
Henrique Jornada <henriquejornada@MacBook-Air-de-Henrique.local>
2025-09-12 19:59:19 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/code_for_fun/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.code_for_fun
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/jornadev/code_for_fun/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jornadev.code_for_fun
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
